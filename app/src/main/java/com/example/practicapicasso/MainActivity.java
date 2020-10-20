ac073ae9d7bce91113d4aa7567b2bb4991a73016
lorena <lorena.albonos@juanxxiii.net>
2020-10-21 01:39:05 +0200

    Terminada con permisos

--- app/src/main/java/com/example/practicapicasso/MainActivity.java
+++ app/src/main/java/com/example/practicapicasso/MainActivity.java
@@ -31,10 +31,12 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
 
     private Button addPermissions, save, download;
-    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1 ;
+    private static final int MY_PERMISSIONS_WRITE_EXTERNAL_STORAGE = 1 ;
     private ImageView mImagen;
     private Timer mTimer = null;
     private TimerTask mTimerTask = null;
+    public final String PERMISO_CONCEDIDO = "Permiso concedido";
+    public final String PERMISO_CONCEDIDO_ANTERIORMENTE = "Permiso concedido en el pasado";
     private int currentTime = 0;
     private static final String URL1 = "https://imagenes.20minutos.es/files/image_656_370/files/fp/uploads/imagenes/2019/10/29/estopa-1.r_d.2052-1554.jpeg";
     private static final String URL2 = "https://musicamalaga.com/wp-content/uploads/2019/05/los-moles2.jpg";
@@ -50,20 +52,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if (ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED) {
+                //Comprobamos si los permisos fueron concedidos anteriormente
+                if(PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)){
 
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    //En caso de que no fueran concedidos con anterioridad mostramos el diálogo para concederlos
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},MY_PERMISSIONS_WRITE_EXTERNAL_STORAGE);
 
-                    } else {
-
-                        ActivityCompat.requestPermissions(MainActivity.this,
-                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
-
-                    }
+                    Toast.makeText(getApplicationContext(), PERMISO_CONCEDIDO ,Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), PERMISO_CONCEDIDO_ANTERIORMENTE ,Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -80,22 +80,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
         save = (Button) findViewById(R.id.save);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                } else {
-
-                }
-                return;
-            }
-        }
     }
 
     public void saveImage(String image) {
